798e296b4a66ada68d7c2086137554b7e347f0f4
yang <yang@getblueshift.com>
2020-01-15 21:21:28 -0800

    fixed a bug in parsing max-sch-timeout

--- apps/nsqd/options.go
+++ apps/nsqd/options.go
@@ -131,6 +131,7 @@ func nsqdFlagSet(opts *nsqd.Options) *flag.FlagSet {
 	flagSet.Duration("max-msg-timeout", opts.MaxMsgTimeout, "maximum duration before a message will timeout")
 	flagSet.Int64("max-msg-size", opts.MaxMsgSize, "maximum size of a single message in bytes")
 	flagSet.Duration("max-req-timeout", opts.MaxReqTimeout, "maximum requeuing timeout for a message")
+	flagSet.Duration("max-sch-timeout", opts.MaxSchTimeout, "maximum delayed timeout for a scheduled message")
 	flagSet.Int64("max-body-size", opts.MaxBodySize, "maximum size of a single command body")
 
 	// client overridable configuration options
