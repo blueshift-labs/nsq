985f57a01fe15f1b27801aeaf151d7ccbffb9e69
yang <yang@getblueshift.com>
2020-01-15 18:30:20 -0800

    add unit tests for badgerq

--- nsqd/channel_test.go
+++ nsqd/channel_test.go
@@ -33,6 +33,28 @@ func TestPutMessage(t *testing.T) {
 	test.Equal(t, msg.Body, outputMsg.Body)
 }
 
+// ensure that we can push a message through a topic and get it out of a channel
+func TestPutMessageBadgerq(t *testing.T) {
+	opts := NewOptions()
+	opts.Logger = test.NewTestLogger(t)
+	_, _, nsqd := mustStartNSQD(opts)
+	defer os.RemoveAll(opts.DataPath)
+	defer nsqd.Exit()
+
+	topicName := "test_put_message" + strconv.Itoa(int(time.Now().Unix()))
+	topic := nsqd.GetTopic(topicName)
+	channel1 := topic.GetChannel("ch#badgerq")
+
+	var id MessageID
+	msg := NewMessage(id, []byte("test"))
+	topic.PutMessage(msg)
+
+	b := <-channel1.backend.ReadChan()
+	outputMsg, _ := decodeMessage(b)
+	test.Equal(t, msg.ID, outputMsg.ID)
+	test.Equal(t, msg.Body, outputMsg.Body)
+}
+
 // ensure that both channels get the same message
 func TestPutMessage2Chan(t *testing.T) {
 	opts := NewOptions()
@@ -44,7 +66,7 @@ func TestPutMessage2Chan(t *testing.T) {
 	topicName := "test_put_message_2chan" + strconv.Itoa(int(time.Now().Unix()))
 	topic := nsqd.GetTopic(topicName)
 	channel1 := topic.GetChannel("ch1")
-	channel2 := topic.GetChannel("ch2")
+	channel2 := topic.GetChannel("ch2#badgerq")
 
 	var id MessageID
 	msg := NewMessage(id, []byte("test"))
@@ -54,7 +76,8 @@ func TestPutMessage2Chan(t *testing.T) {
 	test.Equal(t, msg.ID, outputMsg1.ID)
 	test.Equal(t, msg.Body, outputMsg1.Body)
 
-	outputMsg2 := <-channel2.memoryMsgChan
+	b := <-channel2.backend.ReadChan()
+	outputMsg2, _ := decodeMessage(b)
 	test.Equal(t, msg.ID, outputMsg2.ID)
 	test.Equal(t, msg.Body, outputMsg2.Body)
 }
@@ -137,6 +160,39 @@ func TestChannelEmpty(t *testing.T) {
 	test.Equal(t, int64(0), channel.Depth())
 }
 
+func TestChannelEmptyBadgerq(t *testing.T) {
+	opts := NewOptions()
+	opts.Logger = test.NewTestLogger(t)
+	_, _, nsqd := mustStartNSQD(opts)
+	defer os.RemoveAll(opts.DataPath)
+	defer nsqd.Exit()
+
+	topicName := "test_channel_empty" + strconv.Itoa(int(time.Now().Unix()))
+	topic := nsqd.GetTopic(topicName)
+	channel := topic.GetChannel("channel#badgerq")
+
+	msgs := make([]*Message, 0, 25)
+	for i := 0; i < 25; i++ {
+		msg := NewMessage(topic.GenerateID(), []byte("test"))
+		channel.StartInFlightTimeout(msg, 0, opts.MsgTimeout)
+		msgs = append(msgs, msg)
+	}
+
+	channel.RequeueMessage(0, msgs[len(msgs)-1].ID, 100*time.Millisecond)
+	test.Equal(t, 24, len(channel.inFlightMessages))
+	test.Equal(t, 24, len(channel.inFlightPQ))
+	test.Equal(t, 1, len(channel.deferredMessages))
+	test.Equal(t, 1, len(channel.deferredPQ))
+
+	channel.Empty()
+
+	test.Equal(t, 0, len(channel.inFlightMessages))
+	test.Equal(t, 0, len(channel.inFlightPQ))
+	test.Equal(t, 0, len(channel.deferredMessages))
+	test.Equal(t, 0, len(channel.deferredPQ))
+	test.Equal(t, int64(0), channel.Depth())
+}
+
 func TestChannelEmptyConsumer(t *testing.T) {
 	opts := NewOptions()
 	opts.Logger = test.NewTestLogger(t)
@@ -150,6 +206,43 @@ func TestChannelEmptyConsumer(t *testing.T) {
 	topicName := "test_channel_empty" + strconv.Itoa(int(time.Now().Unix()))
 	topic := nsqd.GetTopic(topicName)
 	channel := topic.GetChannel("channel")
+	client := newClientV2(0, conn, &context{nsqd})
+	client.SetReadyCount(25)
+	err := channel.AddClient(client.ID, client)
+	test.Equal(t, err, nil)
+
+	for i := 0; i < 25; i++ {
+		msg := NewMessage(topic.GenerateID(), []byte("test"))
+		channel.StartInFlightTimeout(msg, 0, opts.MsgTimeout)
+		client.SendingMessage()
+	}
+
+	for _, cl := range channel.clients {
+		stats := cl.Stats()
+		test.Equal(t, int64(25), stats.InFlightCount)
+	}
+
+	channel.Empty()
+
+	for _, cl := range channel.clients {
+		stats := cl.Stats()
+		test.Equal(t, int64(0), stats.InFlightCount)
+	}
+}
+
+func TestChannelEmptyConsumerBadgerq(t *testing.T) {
+	opts := NewOptions()
+	opts.Logger = test.NewTestLogger(t)
+	tcpAddr, _, nsqd := mustStartNSQD(opts)
+	defer os.RemoveAll(opts.DataPath)
+	defer nsqd.Exit()
+
+	conn, _ := mustConnectNSQD(tcpAddr)
+	defer conn.Close()
+
+	topicName := "test_channel_empty" + strconv.Itoa(int(time.Now().Unix()))
+	topic := nsqd.GetTopic(topicName)
+	channel := topic.GetChannel("channel#badgerq")
 	client := newClientV2(0, conn, &context{nsqd})
 	client.SetReadyCount(25)
 	err := channel.AddClient(client.ID, client)
@@ -225,6 +318,47 @@ func TestChannelHealth(t *testing.T) {
 
 	msg = NewMessage(topic.GenerateID(), make([]byte, 100))
 	err = channel.PutMessage(msg)
+	test.NotNil(t, err)
+
+	url := fmt.Sprintf("http://%s/ping", httpAddr)
+	resp, err := http.Get(url)
+	test.Nil(t, err)
+	test.Equal(t, 500, resp.StatusCode)
+	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	test.Equal(t, "NOK - never gonna happen", string(body))
+
+	channel.backend = &errorRecoveredBackendQueue{}
+
+	msg = NewMessage(topic.GenerateID(), make([]byte, 100))
+	err = channel.PutMessage(msg)
+	test.Nil(t, err)
+
+	resp, err = http.Get(url)
+	test.Nil(t, err)
+	test.Equal(t, 200, resp.StatusCode)
+	body, _ = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	test.Equal(t, "OK", string(body))
+}
+
+func TestChannelHealthBadgerq(t *testing.T) {
+	opts := NewOptions()
+	opts.Logger = test.NewTestLogger(t)
+	opts.MemQueueSize = 2
+
+	_, httpAddr, nsqd := mustStartNSQD(opts)
+	defer os.RemoveAll(opts.DataPath)
+	defer nsqd.Exit()
+
+	topic := nsqd.GetTopic("test")
+
+	channel := topic.GetChannel("channel#badgerq")
+
+	channel.backend = &errorBackendQueue{}
+
+	msg := NewMessage(topic.GenerateID(), make([]byte, 100))
+	err := channel.PutMessage(msg)
 	test.NotNil(t, err)
 
 	url := fmt.Sprintf("http://%s/ping", httpAddr)
